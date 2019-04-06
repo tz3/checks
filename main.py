c7ff9795b5ffecf495103681d4bb15da8a247a1d
Akhmetov Rinat <appvshare@gmail.com>
2019-04-06 15:03:31 +0300

    api v1

--- main.py
+++ main.py
@@ -1,40 +1,62 @@
 # -*- coding: utf-8 -*-
-from pprint import pprint
 
+import pyzbar.pyzbar as pyzbar
+from requests import Response
+import cv2
 import requests
 
-authorize_data = {"email": "email@gmail.com", "name": "Your Name", "phone": "+70000000000"}
-password = 000000
-# %%
-# registration
-url = 'https://proverkacheka.nalog.ru:9999/v1/mobile/users/signup'
-r = requests.post(url, json=authorize_data)
-# %%
-# login
-url = 'https://proverkacheka.nalog.ru:9999/v1/mobile/users/login'
-r = requests.get(url, auth=(authorize_data['phone'].encode('utf-8'), password))
-r.status_code
-# %%
-# reset password
-url = 'https://proverkacheka.nalog.ru:9999/v1/mobile/users/restore'
-restore_data = {"phone": "+79033885735"}
-r = requests.post(url, json=restore_data)
-# %%
+BASE_URL = 'https://proverkacheka.nalog.ru:9999/v1'
+authorize_data = {"email": "email", "name": "name", "phone": "phone"}
+username = authorize_data['phone']
+image_path = 'IMAGE 2019-04-06 13:16:58.jpg'
 
-qr_code = 't=20190406T104300&s=641.93&fn=9289000100110273&i=80715&fp=1246602073&n=1'
-t = '20190406T104300'
-s = 641.93
-fn = 9289000100110273
-i = 80715
-fp = 1246602073
-n = 1
 
-fiscalNumber = fn
-fiscalDocument = i
-fiscalSign = fp
-url = f"https://proverkacheka.nalog.ru:9999/v1/inns/*/kkts/*/fss/{fiscalNumber}/tickets/{fiscalDocument}?fiscalSign={fiscalSign}&sendToEmail=no"
+def sign_up(email: str, name: str, phone: str) -> Response:
+    sign_up_url = '/mobile/users/signup'
+    authorize_data: dict = {"email": email, "name": name, "phone": phone}
+    url = BASE_URL + sign_up_url
+    response = requests.post(url, json=authorize_data)
+    return response
 
-headers = {"device-id": '', 'device-os': ''}
-r = requests.get(url, auth=(authorize_data['phone'].encode('utf-8'), password), headers=headers)
-pprint(r.json())
-print(r.text)
+
+def sign_in(username: str, password: str) -> Response:
+    username = username.encode('utf-8')
+    sign_in_url = '/mobile/users/login'
+    url = BASE_URL + sign_in_url
+    response = requests.get(url, auth=(username, password))
+    return response
+
+
+def reset_password(phone: str) -> Response:
+    reset_password_url = '/mobile/users/restore'
+    url = BASE_URL + reset_password_url
+    restore_data = {"phone": phone}
+    response = requests.post(url, json=restore_data)
+    return response
+
+
+def get_information(username: str, password: str, fiscal_number: str, fiscal_document: str,
+                    fiscal_sign: str) -> Response:
+    username = username.encode('utf-8')
+    url = BASE_URL + f'/inns/*/kkts/*/fss/{fiscal_number}/tickets/{fiscal_document}?fiscalSign={fiscal_sign}&sendToEmail=no'
+    headers = {"device-id": '', 'device-os': ''}
+    response = requests.get(url, auth=(username, password), headers=headers)
+    return response
+
+
+def qr_scan(img_path: str) -> pyzbar.Decoded:
+    im = cv2.imread(img_path)
+    decoded_objects = pyzbar.decode(im)
+    return decoded_objects
+
+
+# object_str = qr_scan(image_path)
+def parse_qr_string(line: pyzbar.Decoded) -> tuple:
+    objectives = line[0].data.decode("utf-8").split('&')
+    fn = objectives[2].split('=')[1]
+    i = objectives[3].split('=')[1]
+    fp = objectives[4].split('=')[1]
+    fiscal_number = fn
+    fiscal_document = i
+    fiscal_sign = fp
+    return fiscal_number, fiscal_document, fiscal_sign
